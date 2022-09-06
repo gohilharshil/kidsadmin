e661daec455307b861ee78a9662db64ea01978bf
gohilharshil <myclg.work201@gmail.com>
2022-09-06 21:57:35 +0530

    auth only

---
+++ android/app/src/main/kotlin/com/antinna/kidsadmin/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.antinna.kidsadmin
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/app/features/dashboard/views/screens/dashboard_screen.dart
@@ -0,0 +1,47 @@
+library dashboard;
+
+import 'package:kidsadmin/app/config/routes/app_pages.dart';
+import 'package:kidsadmin/app/shared_components/async_button.dart';
+import 'package:kidsadmin/app/utils/services/service.dart';
+
+import '../../../../shared_components/header_text.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+// binding
+part '../../bindings/dashboard_binding.dart';
+
+// controller
+part '../../controllers/dashboard_controller.dart';
+
+// component
+part '../components/sign_out_button.dart';
+
+class DashboardScreen extends GetView<DashboardController> {
+  const DashboardScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            const HeaderText("dashboard"),
+            const SizedBox(height: 10),
+            _buildSignOutButton(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildSignOutButton() {
+    return Obx(
+      () => _SignOutButton(
+        isLoading: controller.isLoading.value,
+        onPressed: () => controller.signOut(),
+      ),
+    );
+  }
+}
